8d2218961c02238d6a95b8b2eb5c46d72dd5ff9e
arkajitroy <arkajitroy11.official@gmail.com>
2024-10-11 11:26:33 +0530

    refactor: modification in the button variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
-import TestComp from "@/features/test";
 
 export default function Home() {
   return (
-    <main>
-      <h3>Hello World</h3>
-      <Button>Shadcn</Button>
-      <TestComp />
+    <main className="flex gap-2 justify-center flex-wrap">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Desctructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">MUTED</Button>
+      <Button variant="teritory">Teritory</Button>
+      <Button disabled>Disabled</Button>
     </main>
   );
 }
