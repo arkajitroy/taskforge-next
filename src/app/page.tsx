aecc915b068a1bf6ac2d7fc6acca7fb3ae4ef201
arkajitroy <arkajitroy11.official@gmail.com>
2024-10-11 11:32:58 +0530

    modify custom components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,44 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Home() {
   return (
-    <main className="flex gap-2 justify-center flex-wrap">
-      <Button>Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Desctructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">MUTED</Button>
-      <Button variant="teritory">Teritory</Button>
-      <Button disabled>Disabled</Button>
+    <main>
+      <div className="flex gap-2 justify-center flex-wrap">
+        <Button>Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="destructive">Desctructive</Button>
+        <Button variant="ghost">Ghost</Button>
+        <Button variant="muted">MUTED</Button>
+        <Button variant="teritory">Teritory</Button>
+        <Button disabled>Disabled</Button>
+      </div>
+      <h3>Input Component</h3>
+      <Input className="w-1/2" placeholder="Enter the input" />
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a fruit" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Fruits</SelectLabel>
+            <SelectItem value="apple">Apple</SelectItem>
+            <SelectItem value="banana">Banana</SelectItem>
+            <SelectItem value="blueberry">Blueberry</SelectItem>
+            <SelectItem value="grapes">Grapes</SelectItem>
+            <SelectItem value="pineapple">Pineapple</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </main>
   );
 }
--- src/features/test.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function TestComp() {
-  return <div className="text-green-500">test</div>;
-}
