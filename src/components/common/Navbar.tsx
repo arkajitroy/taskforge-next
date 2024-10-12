481fd1a7f4923b5136b24949faa27c11b5eb69e9
arkajitroy <arkajitroy11.official@gmail.com>
2024-10-12 13:05:58 +0530

    refactor: navbar component

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import BrandLogo from "./BrandLogo";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
-const Navbar: React.FC = () => {
+type TNavbarProps = {
+  authButtonTitle: "Sign Up" | "Login";
+  authButtonRedirection: "login" | "register";
+};
+
+const Navbar: React.FC<TNavbarProps> = ({ authButtonTitle, authButtonRedirection }) => {
   return (
     <nav className="flex justify-between items-center">
       <BrandLogo name="brand-logo.svg" title="Taskforge" height={50} width={50} alt="brand-logo" />
-      <Button variant="secondary">Sign Up</Button>
+      <Button variant="secondary" asChild>
+        <Link href={authButtonRedirection}>{authButtonTitle}</Link>
+      </Button>
     </nav>
   );
 };
