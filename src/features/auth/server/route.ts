fcc501f45bbadf0210b83b8c9a6e49f50bf2a004
arkajitroy <arkajitroy11.official@gmail.com>
2024-10-19 13:20:29 +0530

    feat: add login + register configuration

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,16 +1,43 @@
 import { Hono } from "hono";
 import { loginAPIValidator, registerAPIValidator } from "@/validations/routes/auth";
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { setCookie } from "hono/cookie";
+import { AUTH_COOKIE } from "../others/constants";
 
 const app = new Hono()
-  .post("/login", loginAPIValidator, (c) => {
+  .post("/login", loginAPIValidator, async (c) => {
     const { email, password } = c.req.valid("json");
+    const { account } = await createAdminClient();
 
-    return c.json({ data: { email, password }, success: "ok" });
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true });
   })
-  .post("/register", registerAPIValidator, (c) => {
+  .post("/register", registerAPIValidator, async (c) => {
     const { email, name, password } = c.req.valid("json");
+    const { account } = await createAdminClient();
 
-    return c.json({ data: { name, email, password }, success: "ok" });
+    const user = await account.create(ID.unique(), email, password, name);
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ data: user, success: true });
   });
 
 export default app;
