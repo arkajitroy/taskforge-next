dcfa3b9bdcc54398092407258a9e3f3c432b3cdb
arkajitroy <arkajitroy11.official@gmail.com>
2024-10-31 11:00:01 +0530

    feat: add  user-auth button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { useCurrentUser } from "@/features/auth/api/use-current-user";
 import { useLogout } from "@/features/auth/api/use-logout";
 import { LogOut } from "lucide-react";
+import UserAuthButton from "@/features/auth/components/user-button";
 
 export default function Home() {
   const router = useRouter();
@@ -19,9 +20,7 @@ export default function Home() {
   return (
     <main className="flex justify-between py-2 px-4 items-center">
       <span className="text-red-700 font-semibold">Authorized User</span>
-      <Button variant="destructive" className="gap-2" onClick={() => mutate()}>
-        Logout <LogOut size={16} />
-      </Button>
+      <UserAuthButton />
     </main>
   );
 }
