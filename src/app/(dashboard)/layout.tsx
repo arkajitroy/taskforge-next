c0b8b084d471fa98e039f00c7f15dfc4796a7fa5
arkajitroy <arkajitroy11.official@gmail.com>
2024-11-01 20:50:03 +0530

    refactor: initial setup of dashboard layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,21 @@
+import React, { PropsWithChildren } from "react";
+import Sidebar from "@/components/common/sidebar";
+
+export default function Dashboardlayout({ children }: PropsWithChildren) {
+  return (
+    <div className="min-h-screen">
+      <div className="flex w-full h-full ">
+        <div className="h-full overflow-y-auto fixed left-0 top-0 hidden lg:block lg:w-[264px]">
+          <Sidebar />
+        </div>
+
+        <div className="lg:pl-[264px]">
+          <div className="mx-auto max-w-screen-2xl h-full">
+            {/* // todo : Add Navbar  */}
+            <main className="h-full py-8 px-6 flex flex-col">{children}</main>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
