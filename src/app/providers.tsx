bc8d7e377fdc0e24317d32350184ad9ad589d8f3
arkajitroy <arkajitroy11.official@gmail.com>
2024-11-10 21:26:32 +0530

    feat: add required library for workspace-creation

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,14 +4,17 @@ import React, { PropsWithChildren } from "react";
 import { QueryProvider } from "@/providers/query-provider";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
     <QueryProvider>
-      <TooltipProvider>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </TooltipProvider>
+      <NuqsAdapter>
+        <TooltipProvider>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </TooltipProvider>
+      </NuqsAdapter>
     </QueryProvider>
   );
 }
