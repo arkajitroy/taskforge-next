39687e067915679c98e0a7eca17466a09fe86b8c
arkajitroy <arkajitroy11.official@gmail.com>
2024-11-16 14:42:55 +0530

    refactor: query functionality convention

--- src/features/workspaces/queries/index.ts
+++ src/features/workspaces/queries/index.ts
@@ -6,6 +6,7 @@ import { cookies } from "next/headers";
 import { Account, Client, Databases, Query } from "node-appwrite";
 import { getMemberService } from "../services";
 import { TWorkspace } from "../others/types";
+import { createSessionClient } from "@/lib/appwrite";
 
 // prop types
 type TGetWorkspaceActionProps = {
@@ -14,18 +15,7 @@ type TGetWorkspaceActionProps = {
 
 export const getCurrentWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-    const session = await cookies().get(AUTH_COOKIE);
-
-    if (!session) return { documents: [], total: 0 };
-
-    client.setSession(session.value);
-
-    const databases = new Databases(client);
-    const account = new Account(client);
+    const { account, databases } = await createSessionClient();
     const user = await account.get();
 
     const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
