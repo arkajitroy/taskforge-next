dc4cc3bb653c849264901f55e1e87208c7750335
arkajitroy <arkajitroy11.official@gmail.com>
2024-11-17 10:26:02 +0530

    feat: add custom utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,6 @@
+import { TWorkspace } from "@/features/workspaces/others/types";
 import { clsx, type ClassValue } from "clsx";
+import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -15,4 +17,15 @@ export function generateInvitationCode(length: number): string {
   }
 
   return invitationCode;
+}
+
+export function generateInviteCodeLink(initialValues: TWorkspace): string {
+  const fullLink = `${window.location.origin}/workspaces/${initialValues.$id}/join/${initialValues.inviteCode}`;
+  return fullLink;
+}
+
+export function copyToClipboard(parsedText: string) {
+  navigator.clipboard
+    .writeText(parsedText)
+    .then(() => toast.success("Invite link copied to clipboard!"));
 }
